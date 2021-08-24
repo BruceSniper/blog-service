09093ded8d415cb838eca9dae2db8dcf5b49dae9
zhangyuyao <zhangyuyao@shein.com>
2021-08-24 11:33:34 +0800

    feat: add pagination

--- internal/model/model.go
+++ internal/model/model.go
@@ -8,6 +8,17 @@
 
 package model
 
+import (
+	"blog-service/global"
+	"blog-service/pkg/setting"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+)
+
 type Model struct {
 	ID         uint32 `gorm:"primary_key" json:"id"`
 	CreatedBy  string `json:"created_by"`
@@ -16,4 +27,37 @@ type Model struct {
 	ModifiedOn uint32 `json:"modified_on"`
 	DeletedOn  uint32 `json:"deleted_on"`
 	IsDel      uint8  `json:"is_del"`
+}
+
+func NewDBEngine(databaseSetting *setting.DatabaseSettingS) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=%s&parseTime=%t&loc=Local",
+		databaseSetting.UserName,
+		databaseSetting.Password,
+		databaseSetting.Host,
+		databaseSetting.DBName,
+		databaseSetting.Charset,
+		databaseSetting.ParseTime,
+	)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if global.ServerSetting.RunMode == "debug" {
+		db.Debug()
+	}
+
+	DB, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+	DB.SetMaxIdleConns(databaseSetting.MaxIdleConns)
+	DB.SetMaxOpenConns(databaseSetting.MaxOpenConns)
+
+	return db, nil
 }
---
+++ pkg/app/app.go
@@ -0,0 +1,57 @@
+/**
+ * @Author:      zhangyuyao
+ * @Description: TODO
+ * @File:        app.go
+ * @Version:     1.0.0
+ * @Date:        2021/8/24 11:23 上午
+ */
+
+package app
+
+import (
+	"blog-service/pkg/errcode"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Response struct {
+	Ctx *gin.Context
+}
+
+type Pager struct {
+	Page      int `json:"page"`
+	PageSize  int `json:"page_size"`
+	TotalRows int `json:"total_rows"`
+}
+
+func NewResponse(ctx *gin.Context) *Response {
+	return &Response{Ctx: ctx}
+}
+
+func (r *Response) ToResponse(data interface{}) {
+	if data == nil {
+		data = gin.H{}
+	}
+	r.Ctx.JSON(http.StatusOK, data)
+}
+
+func (r *Response) ToResponseList(list interface{}, totalRows int) {
+	r.Ctx.JSON(http.StatusOK, gin.H{
+		"list": list,
+		"page": Pager{
+			Page:      GetPage(r.Ctx),
+			PageSize:  GetPageSize(r.Ctx),
+			TotalRows: totalRows,
+		},
+	})
+}
+
+func (r *Response) ToErrorResponse(err *errcode.Error) {
+	response := gin.H{"code": err.Code(), "msg": err.Msg()}
+	details := err.Details()
+	if len(details) > 0 {
+		response["details"] = details
+	}
+	r.Ctx.JSON(err.StatusCode(), response)
+}
